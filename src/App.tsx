2a5c8cfa6c745c5321008a0c80148dc82b66ba76
MariaHellsen <maria.hellsen@outlook.com>
2025-08-20 20:31:17 +0200

    Added router including layout and files components: header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import "./css/App.css";
+import { router } from "./Router";
+import { RouterProvider } from "react-router";
 
 function App() {
   return (
     <>
+      <RouterProvider router={router}></RouterProvider>
       <h1>Case 1, grupp 3</h1>
     </>
   );
