bce36ff47f7568494f9938bb53e3217ff2e59577
FrejaE <freja.edberg@outlook.com>
2025-08-25 21:56:24 +0200

    description med mocupdata

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -7,14 +7,33 @@ import {
   DigiTypography,
   DigiTypographyMeta,
 } from "@digi/arbetsformedlingen-react";
-
+import { useContext, useEffect } from "react";
+import { EducationContext } from "../context/EducationsContext";
+// kommer behöva en prop för education
 export const Description = () => {
+  const { educations, fetchEducations } = useContext(EducationContext);
+
+  useEffect(() => {
+    fetchEducations("förskollärare");
+  }, []);
+
+  const education = educations[0];
+  console.log("HÄÄÄR", education);
+
+  const titles = education?.education?.title ?? null;
+  const findedTitle = titles?.find((i) => i.lang === "swe");
+  const title = findedTitle?.content;
+  console.log("title: ", title);
+
   return (
     <>
       <DigiLayoutBlock afVariation={LayoutBlockVariation.PRIMARY}>
         <DigiTypography>
           <h2>Skolnamn plus ev icon</h2>
-          <p> utbildning, title från Education</p>
+          <p> utbildning, {title ? title : "ingen titel"}</p>
+          <p> utbildning, {title ?? "ingen titel"}</p>
+          <p> utbildning, {title && title}</p>
+          <p> utbildning, {title}</p>
           <p>skola plus plats</p>
           <DigiTypographyMeta afVariation={TypographyMetaVariation.PRIMARY}>
             <p>Beskrivning av utbildningen</p>
--- src/pages/Educations.tsx
+++ src/pages/Educations.tsx
@@ -1,6 +1,7 @@
 import { useSearchParams } from "react-router";
 import { useContext, useEffect } from "react";
 import { EducationContext } from "../context/EducationsContext";
+import { Description } from "../components/Description";
 
 export const Educations = () => {
   const [searchParams] = useSearchParams();
@@ -14,7 +15,7 @@ export const Educations = () => {
 
   return (
     <>
-      <h1>Utbildningar</h1>
+      {/* <h1>Utbildningar</h1>
       <p>Sökresultat för: {query}</p>
       {loading && <p>Laddar...</p>}
       {error && <p>Det gick inte att hämta utbildningar.</p>}
@@ -22,7 +23,8 @@ export const Educations = () => {
         {educations.map((e) => (
           <li key={e.id}>{e.education?.title?.[0]?.content || ""}</li>
         ))}
-      </ul>
+      </ul> */}
+      <Description />
     </>
   );
 };
