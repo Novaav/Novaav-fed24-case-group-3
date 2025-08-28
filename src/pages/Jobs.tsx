1909fede57b9052ff0f6dec0dce107a185902504
gutamuw <129604444+gutamuw@users.noreply.github.com>
2025-08-28 18:05:55 +0200

    glömde få med loadern...

--- src/pages/Jobs.tsx
+++ src/pages/Jobs.tsx
@@ -2,17 +2,22 @@ import { useContext, useEffect } from "react";
 import { JobsContext } from "../context/JobsContext";
 import { MasterDetailLayout } from "./MasterDetailLayout";
 import { Link, useNavigate, useSearchParams } from "react-router";
-import { DigiButton, DigiInfoCardMulti } from "@digi/arbetsformedlingen-react";
+import {
+  DigiButton,
+  DigiInfoCardMulti,
+  DigiLoaderSpinner,
+} from "@digi/arbetsformedlingen-react";
 import {
   ButtonSize,
   ButtonVariation,
   InfoCardMultiHeadingLevel,
   InfoCardMultiType,
+  LoaderSpinnerSize,
 } from "@digi/arbetsformedlingen";
 import "../css/Jobs.css";
 
 export const Jobs = () => {
-  const { jobs, fetchJobs } = useContext(JobsContext);
+  const { jobs, fetchJobs, loading } = useContext(JobsContext);
   const [searchParams] = useSearchParams();
   const query = searchParams.get("query") || "";
   const navigator = useNavigate();
@@ -32,37 +37,43 @@ export const Jobs = () => {
         {`Gå tillbaka`}
       </DigiButton>
       <div className="jobs-container">
-        {jobs.map((job) => (
-          <DigiInfoCardMulti
-            afHeading={job.headline}
-            afHeadingLevel={InfoCardMultiHeadingLevel.H5}
-            afType={InfoCardMultiType.RELATED}
-            key={job.id}
-            className="job-card"
-          >
-            {job.logo_url && (
-              <img
-                src={job.logo_url}
-                alt={`${job.employer?.name} logotyp`}
-                className="job-logo"
-              />
-            )}
-            <div className="job-ad-details">
-              <div>
-                <span className="employer-label">Företag:</span>{" "}
-                <span className="employer">{job.employer?.name}</span>
+        {loading ? (
+          <DigiLoaderSpinner
+            afSize={LoaderSpinnerSize.MEDIUM}
+            afText="Laddar"
+          />
+        ) : (
+          jobs.map((job) => (
+            <DigiInfoCardMulti
+              afHeading={job.headline}
+              afHeadingLevel={InfoCardMultiHeadingLevel.H5}
+              afType={InfoCardMultiType.RELATED}
+              key={job.id}
+              className="job-card fade-in"
+            >
+              {job.logo_url && (
+                <img
+                  src={job.logo_url}
+                  alt={`${job.employer?.name} logotyp`}
+                  className="job-logo"
+                />
+              )}
+              <div className="job-ad-details">
+                <div>
+                  <span className="employer-label">Företag:</span>{" "}
+                  <span className="employer">{job.employer?.name}</span>
+                </div>
+                <div>
+                  <span className="address-label">Stad:</span>{" "}
+                  <span className="address">
+                    {job.workplace_address?.municipality}
+                  </span>
+                </div>
               </div>
-              <div>
-                <span className="address-label">Stad:</span>{" "}
-                <span className="address">
-                  {job.workplace_address?.municipality}
-                </span>
-              </div>
-            </div>
-
-            <Link to={`/jobs/${job.id}`}>Läs mer</Link>
-          </DigiInfoCardMulti>
-        ))}
+              <Link to={`/jobs/${job.id}`}>Läs mer</Link>
+            </DigiInfoCardMulti>
+          ))
+        )}
       </div>
     </MasterDetailLayout>
   );
